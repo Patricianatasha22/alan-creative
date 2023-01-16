e11811c752d16aec7138f1de1f4c99b5115f7055
Patricianatasha22 <patricianatasha01@gmail.com>
2023-01-16 11:30:15 +0800

    final
    email not working

--- website/website/settings.py
+++ website/website/settings.py
@@ -125,9 +125,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nextlevelt05@gmail.com'
-EMAIL_HOST_PASSWORD = 'zsccbtvstxjmoocx'
+EMAIL_HOST_PASSWORD = 'zcqbuisvdxutqoxy'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 
 # MEDIA
 MEDIA_URL = "/media/"
